b95138361229462806fe870345f1b8f899056870
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2025-09-27 17:35:27 -0600

    Add skeleton

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://example.com',
+
+  vite: {
+    build: {
+      // Limit inline css (default is...lower)
+      assetsInlineLimit: 14000,
+    },
+  },
+
+  // https://docs.astro.build/en/guides/prefetch/
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: 'viewport'
+  },
+
+  integrations: [sitemap()],
+});
---
+++ src/data/config.js
@@ -0,0 +1,9 @@
+export const site = {
+  title: "",
+  description: "",
+  author: "",
+};
+
+export const links = {
+  email: "samfeldstein@pm.me"
+}
