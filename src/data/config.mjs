8eeea67fa136d83ad0af6fd19793e3bba16ab1c2
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2026-02-26 12:12:15 -0700

    Update base layout and head

--- src/data/config.mjs
+++ src/data/config.mjs
@@ -1,10 +1,15 @@
 export const site = {
   title: "Astro Site Template",
   description: "A basic Astro starter site.",
-  author: "Sam Feldstein",
   copyright: "Sam Feldstein"
 };
 
-export const links = {
-  email: "samfeldstein@pm.me"
+export const me = {
+  name: "Sam Feldstein",
+  email: "samfeldstein@pm.me",
+};
+
+export const client = {
+  name: "",
+  email: "client@email.com"
 }
